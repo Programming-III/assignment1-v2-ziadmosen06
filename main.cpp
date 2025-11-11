6d8c506725d8e250f2a92c5b039525ea3b50cade
Ziad_Mohsen <125408057+ziadmosen06@users.noreply.github.com>
2025-11-11 09:43:53 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -11,29 +11,58 @@ using namespace std;
 // ==================== Person Class Implementation =========================
 
 
-
-
-
-
+ Person :: Person(string name , int id) {
+    this -> name = name;
+    this -> id = id;
+}
+void Person :: display() {
+     cout << "Name: " << name <<"ID: " << id << endl;
+ }
 
 
 // ==================== Student Class Implementation ====================
 
 
-
+Student :: Student(int yearLevel , string major) : Person( string name ,int id) {
+     this-> yearLevel = yearLevel;
+     this->major = major;
+ }
+void Student :: display() {
+     cout <<"yearLevel" << yearLevel << "major" << major << endl;
+ }
 
 
 
 // ==================== Instructor Class Implementation ====================
 
 
-
-
+Instructor :: Instructor(string department , int experienceYears) : Person( string name ,int id) {
+    this->department = department;
+    this->experienceYears = experienceYears;
+}
+void Instructor:: display() {
+     cout <<"department" << department << "experienceYears" << experienceYears << endl;
+ }
 
 
 // ==================== Course Class Implementation ====================
 
+Course :: Course(string courseCode , string courseName , int maxStudents , Student* students , int currentStudent) {
+     this->courseCode = courseCode;
+     this->courseName = courseName;
+     this->maxStudents = maxStudents;
+     this->students = students;
+     this->currentStudent = currentStudent;
 
+ }
+void Course::displayCourseInfo() {
+    cout <<"courseCode" << courseCode << endl;
+     cout <<"courseName" << courseName << endl;
+     cout <<"maxStudents" << maxStudents << endl;
+     cout <<"students" << students << endl;
+     cout <<"currentStudent" << currentStudent << endl;
+     
+}
 
 
 
