0431a1afe3e476eee5a5f7d5c137dde44a94486e
Ziad_Mohsen <125408057+ziadmosen06@users.noreply.github.com>
2025-11-11 09:52:52 +0200

    Student.h

--- Student.h
+++ Student.h
@@ -5,16 +5,18 @@
 #include <string>
 #using namespace std;
 //#write student class here
-class Student : Person{
-public:
+class Student : public Person{
+private:
     int yearLevel;
     string major;
 private:
     Student(int yearLevel, string major);
     void display();
+    void getMajor();
+    void setMajor(string major);
+    void setYearLevel(int yearLevel);
+    void getYearLevel();
 };
-
-
 
 
 
