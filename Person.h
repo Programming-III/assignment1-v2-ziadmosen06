d955d9d832d1ddb470ed9761f464a87b7063e679
Ziad_Mohsen <125408057+ziadmosen06@users.noreply.github.com>
2025-11-11 09:53:12 +0200

    Person.h

--- Person.h
+++ Person.h
@@ -6,14 +6,19 @@ using namespace std;
 
 //#write Person class here
 class Person {
-    public:
+    private:
     string name;
     int id;
     private:
-    Person(string name, int id);
-    void display();
-};
 
+    Person(string name , int id);
+    void display();
+    void getName();
+    void getId();
+    void setName(string name);
+    void setId(int id);
+
+};
 
 
 
