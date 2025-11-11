bc27fa13e31412e11d249b45adaf88ff63f30b98
Ziad_Mohsen <125408057+ziadmosen06@users.noreply.github.com>
2025-11-11 09:51:01 +0200

    Course.h

--- Course.h
+++ Course.h
@@ -5,8 +5,8 @@
 #include "Student.h"
 using namespace std;
 //#write your code here
-class Course {
-public:
+class Course : Student{
+private:
     string courseCode;
     string courseName;
     int maxStudents;
@@ -15,8 +15,16 @@ public:
     private:
     void addStudents(const Student& s);
     void displayCourseInfo();
+    void getCourseCode();
+    void getCourseName();
+    void getMaxStudents();
+    void getStudents();
+    void getCurrentStudent();
+    void setCurrentStudent(int currentStudent);
+    void setMaxStudents(int maxStudent);
+    void setCourseName(string courseName);
+    void setCourseCode(string courseCode);
 };
-
 
 
 
